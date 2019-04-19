bbe0d4eac413bfbc4d5722b9957fbddab68fd29a
vilnyus <govagharsh@gmail.com>
2019-04-19 14:42:25 +0400

    Added rooms javascript typescript css

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-var path = require('path');
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
@@ -13,13 +12,30 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/public/index.html');
 });
 
+app.get('/javascript.html', (req, res)=>{
+    res.sendFile(__dirname + '/public/javascript.html');
+});
+
+app.get('/typescript.html', (req, res)=>{
+    res.sendFile(__dirname + '/public/typescript.html');
+});
+
+app.get('/css.html', (req, res)=>{
+    res.sendFile(__dirname + '/public/css.html');
+});
+
 const tech = io.of('/tech');
 
 tech.on('connection', (socket) => {
-    console.log('user connected');
-    socket.on('message', (msg)=> {
-        console.log(`message: ${msg}`);
-        tech.emit('message', msg);
+    socket.on('join', (data)=>{
+        socket.join(data.room);
+        tech.in(data.room).emit('message', `New user connected to room ${data.room}!`);
+        console.log(`New user connected to room ${data.room}!`);
+    })
+    
+    socket.on('message', (data)=> {
+        tech.in(data.room).emit('message', data.msg);        
+        console.log(`message: ${data.msg}`);
     });
 
     socket.on('disconnect', ()=>{
